47a1e99b3d7e38895388f55eb6a2b8db395a316a
jwlerch78 <jwlerch@gmail.com>
2025-08-22 16:43:06 -0400

    Update rightpanel.js

--- js/rightpanel.js
+++ js/rightpanel.js
@@ -20,4 +20,9 @@ function showContainer(id) {
     const el = document.getElementById(c);
     if (el) el.style.display = (c===id) ? "block" : "none";
   });
+    // toggle header container only if calendar is active
+  const header = document.getElementById("header-container");
+  if (header) {
+    header.style.display = (id === "calendar-container") ? "block" : "none";
+  }
 }
