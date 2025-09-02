ac9f56ee68a30ff639a110dcf52ce72e2b87fb60
jwlerch78 <jwlerch@gmail.com>
2025-09-02 15:39:24 -0400

    Update location.js

--- js/location.js
+++ js/location.js
@@ -173,10 +173,7 @@ async function updateLocations() {
       // Notify location bar widget
 
 const widgetFrame = document.getElementById("location-frame"); 
-  console.log("widgetFrame initialized");
-
 if (widgetFrame && widgetFrame.contentWindow) {
-  console.log("Sending Post Message");
   widgetFrame.contentWindow.postMessage(
     {
       type: "locationUpdate",
