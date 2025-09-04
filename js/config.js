dccf0e572d99805a74ef8c598713a5023dbe4d03
jwlerch78 <jwlerch@gmail.com>
2025-09-04 09:42:14 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// ****************
+// config.js
+//*****************
+
+// Theme + configuration
+const theme = {
+  sidebarBg: "#222",
+  iconColor: "white",
+  selectedOutline: "yellow",
+  focusedOutline: "cyan"
+};
+
+const sidebarOptions = [
+  { id: "reload", iconSrc: "icons/icon-reload.svg" },
+  { id: "calendar", iconSrc: "icons/icon-calendar.svg" },
+  { id: "map", iconSrc: "icons/icon-map.svg" },
+  { id: "camera", iconSrc: "icons/icon-video-camera.svg" },
+  { id: "separator", type: "separator" }, // special separator item
+  { id: "settings", iconSrc: "icons/icon-settings.svg" },
+  { id: "exit", iconSrc: "icons/icon-exit.svg" }
+];
