9eec0165599cd2b55adb1672d2c9405595d94bf5
jwlerch78 <jwlerch@gmail.com>
2025-09-19 16:01:39 -0400

    Update calendar.js

--- widgets/calendar/calendar.js
+++ widgets/calendar/calendar.js
@@ -199,6 +199,7 @@ async initializeCalendar() {
         workweek: false,
         hourStart: 6,
         hourEnd: 24,
+        hourHeight: 24,
         showNowIndicator: true,
         eventView: ['time', 'allday'],
         taskView: false
